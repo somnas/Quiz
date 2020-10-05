da96c724c6a086bdba95f5f801bc8dd8316d74d5
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-10-05 14:59:39 +0200

    revert test back to master

--- script.js
+++ script.js
@@ -1,134 +1,113 @@
-/* let question = document.getElementById("question");
-let choices = Array.from(document.getElementsByClassName("choice-text"));
-let correct_bonus = 10;
-let max_questions = 3;
-let question_counter_text = document.getElementById("question-counter");
-let score_text = document.getElementById("score");
-
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let availableQuesions = [];
 
 let questions = [];
 
-(async fetchData() 
-    let url = "https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10";
-    let data = await (await fetch(url)).json();
-    data = questions;
-    console.log(data);
+fetch(
+    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
 
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
 
-fetchData();
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
 
-fetch("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10")
-.then(res => {
-    return res.json();
-})
-.then(loadedQuestions => {
-    questions = loadedQuestions;
-    //console.log(choices);
-    
-    //console.log(questions[2].answers.answer_a);
-    startGame();
-}) 
+            return formattedQuestion;
+        });
 
-console.log(questions);
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
-
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
-
     questionCounter = 0;
     score = 0;
-    score_text.innerHTML = score;    
-    availableQuestions = [...questions];
+    availableQuesions = [...questions];
     getNewQuestion();
-}
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
+};
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter >= max_questions) {
-        return window.location.assign("/end.html");
-    } 
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
     questionCounter++;
-    question_counter_text.innerText = questionCounter + "/" + max_questions;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
     question.innerHTML = currentQuestion.question;
 
-    
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerHTML = currentQuestion['choice' + number];
+    });
 
-    choices.forEach(choice => {
-        
-        const letters = choice.dataset["letter"];
-
-        console.log(letters);
-        
-        //const answers = Array.from(currentQuestion.answers);
-
-        
-        //console.log(answers);
-        //const number = choice.dataset["number"];
-        //choice.innerText = currentQuestion.answers.answer + letters; 
-    })
-
-    availableQuestions.splice(questionIndex, 1);
+    availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return;
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        const selectedAnswer = selectedChoice.dataset['number'];
 
-        let classToApply = "incorrect";
-        if(selectedAnswer == currentQuestion.answer) {
-            classToApply = "correct";
-            score++;
-            score_text.innerHTML = score;
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
         }
 
         selectedChoice.parentElement.classList.add(classToApply);
 
-        setTimeout( () => {
+        setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
         }, 1000);
-        
-        
-    })
-}) */
+    });
+});
 
-
-class Game {
-
-}
-
-class Question {
-
-}
-
-class Questions {
-    constructor() {
-        
-        async fetchData() 
-            await fetch("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10")
-            .then(res => res.json())
-            .then(data => questions = Array.from(data));
-            console.log(questions);
-        
-
-    }
-    
-}
-
-class Score {
-
-}
-
-
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+};
