f95089a636afec194dc63ec046efc377ea59a2ba
Erik Somnäs <erik.somnas@gmail.com>
2020-10-05 18:08:59 +0200

    fetching, collecting and coverting data

--- script.js
+++ script.js
@@ -11,16 +11,27 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [];
+
 
 async function fetchData() {
-    const res = await fetch("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=1")
+    const res = await fetch("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10")
     const data = await res.json();
     
-    console.log(data);
+    /* console.log(data);
     console.log(data[0].question);
     console.log(data[0].answers);
     console.log(data[0].answers.answer_a);
+    console.log(data.length); */
+
+    console.log(data);
+
+    let questions = [];
+
+    /* for(i = 0; i < data.length; i++) {
+        questions.push(data[i].question);
+        
+    }
+    console.log(questions); */
 }
     
 fetchData();
