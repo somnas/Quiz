50f6c9e5f625bd6c28cd3df0c26c73c0e295cd12
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-10-06 12:32:00 +0200

    building questions class

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const question = document.getElementById('question');
+/* const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
@@ -9,29 +9,47 @@ let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuesions = [];
+let availableQuesions = []; */
 
 
 
 
-
-async function fetchData() {
-    const res = await fetch("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=1")
-    const data = await res.json();
-
-   
-
-    
-     data.forEach((object) => {
-        object.isCurrentQuestion = false;
-        object.playerChoice = object.correct_answers;
+class Questions {
+    constructor() {
+        this.currentQuestion = {};
         
-         for (const value in object.playerChoice) {
-            object.playerChoice[value] = "false";
-        }  
-    });  
+    }
 
-    
+    async fetchData() {
+
+        try {
+            const res = await fetch("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10");
+            const data = await res.json();
+        
+            data.forEach((object) => {
+            object.isCurrentQuestion = false;
+            object.playerChoice = object.correct_answers;
+            
+            for (const value in object.playerChoice) {
+                object.playerChoice[value] = "false";
+                }  
+            });  
+            } catch (error) {
+                console.log(error);
+            }
+        data[0].isCurrentQuestion = true;
+        this.currentQuestion = data[0];
+        this.array = data;
+        return data;
+        
+    }
+}
+
+let questions = new Questions();
+
+console.log(questions);
+        
+
       //questions = data.map(loadedQuestions => {
        // const formattedQuestions = [
         //    /*question:*/ loadedQuestions            
@@ -62,9 +80,9 @@ async function fetchData() {
         
     }
     console.log(questions); */
-}
+
     
-fetchData();
+
    
 
     
