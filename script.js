c7640c4aef562513ddeccbad4ed18a3649097f83
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-10-07 18:50:51 +0200

    building classes

--- script.js
+++ script.js
@@ -1,116 +1,28 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull');
-const loader = document.getElementById('loader');
-const game = document.getElementById('game');
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuesions = [];
+document.addEventListener("DOMContentLoaded", () => {
+    
+    new LoadQuestions();
 
-let questions = [];
+    let playBtn = document.getElementById("playBtn");
+    playBtn.addEventListener("click", newPlayer);
 
-fetch(
-    "https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10"
-)
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-
-
-            
-            /* const formattedQuestion = {
-                question: loadedQuestion.question, */
-            };
-
-            /* const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
-
-            return formattedQuestion; */
-        });
-
-        startGame();
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
-//CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
-
-startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuesions = [...questions];
-    getNewQuestion();
-    game.classList.remove('hidden');
-    //loader.classList.add('hidden');
-};
-
-getNewQuestion = () => {
-    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-        //go to the end page
-        return window.location.assign('/end.html');
+    function newPlayer(player) {
+       new Player;
+       return player;
     }
-    questionCounter++;
-    //progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    //Update the progress bar
-    //progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    console.log(player);
+})
 
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerHTML = currentQuestion.question;
 
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerHTML = currentQuestion['choice' + number];
-    });
 
-    availableQuesions.splice(questionIndex, 1);
-    acceptingAnswers = true;
-};
+class Player {
+    constructor(name) {
+        this.name = document.getElementById("player-name").value;
+        this.score = 0;
+        console.log(this);
+    }
+    
+}
 
-choices.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
-        if (!acceptingAnswers) return;
+class LoadQuestions {
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-
-        const classToApply =
-            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-        if (classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
-    });
-});
-
-incrementScore = (num) => {
-    score += num;
-    scoreText.innerText = score;
-};
+}
