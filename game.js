6d1e776f8e13a7d72b2a3ee82c9654ecf197dfff
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-10-07 19:30:34 +0200

    added player name in local storage and api function to class

--- game.js
+++ game.js
@@ -6,4 +6,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
 class LoadQuestions {
 
-}
+    async function fetchData(url) {
+        //let url = ;
+        const res = await fetch(url)
+        const data = await res.json();
+       
+        return data;
+    }
+    
+    async function loadQuestions() {
+        let loadedQuestions = await fetchData("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10");
+    
+        let question_text = document.getElementById("question");
+        let answers_text = Array.from(document.getElementsByClassName("choice-text"));
+        let load_quest_arr = Array.from((Object.values(loadedQuestions)))
+        question_text.innerHTML = loadedQuestions[0].question;
+        
+        answers_text.forEach(function(answer) {
+            answer.added = true;
+            
+        })
+    
+        
+    
+        for(let i = 0; i < loadedQuestions.length; i++) {
+            console.log(loadedQuestions[i].answers);
+        }
+    
+    }
+
+
+
+
