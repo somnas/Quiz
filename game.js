39b8bb3a6f14fdc7f68b046cc5c78982b11617b4
Erik Somnäs <erik.somnas@gmail.com>
2020-10-11 19:56:28 +0200

    moving in n out of functions

--- game.js
+++ game.js
@@ -22,26 +22,28 @@ class LoadQuestions {
     async function loadQuestions() {
         let loadedQuestions = await fetchData("https://quizapi.io/api/v1/questions?apiKey=ZaUKDEKaG02Lc411UFJe6pGrhokdPQw8TjZxlaTk&limit=10");
         
-        getNewQuestion();
-
-        function getNewQuestion() {
         let choice1 = document.getElementById("choice1");
         let choice2 = document.getElementById("choice2");
         let choice3 = document.getElementById("choice3");
         let choice4 = document.getElementById("choice4");
         let choice5 = document.getElementById("choice5");
         let choice6 = document.getElementById("choice6");
-
+        
         let question_counter = document.getElementById("question-counter");
         question_counter.innerHTML = 1;
         let q = question_counter.innerHTML
         let question_text = document.getElementById("question");
         let answers_text = Array.from(document.getElementsByClassName("choice-text"));
         let load_answ_arr = (Object.values(loadedQuestions[q-1].answers));
+
+       
+
         //let load_corr_answ_arr = (Object.values(loadedQuestions[i-1].correct_answers));
         let curr_question = {};
         curr_question = loadedQuestions[q-1];
         let answer_obj = {};
+
+        
         let user_answers = {
             answer_a_correct: "false",
             answer_b_correct: "false", 
@@ -51,16 +53,14 @@ class LoadQuestions {
             answer_f_correct: "false"
         }
         answer_obj = loadedQuestions[0].answers
-
-        //console.log(answer_obj);
-     
+        
         question_text.innerHTML = loadedQuestions[q-1].question;   
         
         for(i = 0; i < load_answ_arr.length; i++) {
             answers_text[i].innerHTML = load_answ_arr[i];
             //add if-statement to hide empty choices
         }
- 
+
         choice1.addEventListener("click", function() {
             user_answers.answer_a_correct = "true";
         })
@@ -87,8 +87,18 @@ class LoadQuestions {
 
         let nextQuestBtn = document.getElementById("next-question");
         nextQuestBtn.addEventListener("click", function() {
+            getNewQuestion();
             //update counter to display next question
         })
+
+        //getNewQuestion();
+
+        function getNewQuestion() {
+    
+            
+
+            
+
         }
         
 
